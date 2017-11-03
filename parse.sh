662b3481610e916bddaaf61aec9ef86f3b050b06
egee-irl <brian@egee.io>
2017-11-03 08:02:22 -0700

    bash, not so much

---
+++ parse.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "ip number1 number2 dateto datefrom" >> headers
+curl https://isc.sans.edu/ipsascii.html | tee ips
+sed -i -r '/(#.*)/d' ips # Probably don't need two different temp files /shrug
+cat ips >> headers
+sed -i 's/\s/,/g' headers
+cat headers | perl -pe 's/(?<!\d)0+//g' > ips.csv # This regex is a little overzealous but, OK
+rm headers && rm ips
