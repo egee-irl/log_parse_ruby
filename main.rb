85cb9f69f8b3b1cb79cff0f2c77fe95ff0a903da
egee-irl <brian@egee.io>
2017-11-06 05:59:43 -0800

    Print failure uri's with res codes

--- main.rb
+++ main.rb
@@ -16,13 +16,11 @@ abort('Log file must be a web log!') unless logs.last.match?(ip_match)
 
 # List variables for clarity
 success = 0
-redirect = 0
-failure = 0
-failure_code = nil
+redirects = 0
+failures = 0
 failure_uri = []
 ip_addresses = []
 res_sizes = []
-
 
 # Since our logs variable is an array, let's dive in!
 logs.each do |line|
@@ -34,20 +32,25 @@ logs.each do |line|
 
   res_sizes.push(line.match(resource_size)[0].to_i)
   success += 1 if line.match?(two_hundred)
-  redirect += 1 if line.match?(three_hundred)
+  redirects += 1 if line.match?(three_hundred)
 
   next unless line.match?(four_hundred)
-  failure_uri.push(line.match(fail__uri))
-  failure_code = line.match(four_hundred).to_s.chop
-  failure += 1
+  failure_uri.push(line.match(fail__uri).to_s +
+  " Response Code:#{line.match(four_hundred).to_s.chop}")
+  failures += 1
 end
 
 puts "Total IPs: #{ip_addresses.size}\n"
 puts "Unique IPs: #{ip_addresses.uniq.size}\n"
-puts "Success Codes: #{success}\n"
-puts "Redirect Codes: #{redirect}\n"
-puts "Failure Codes: #{failure}\n"
 puts "Largest resource size in bytes: #{res_sizes.max}\n"
 puts "Smallest resource size in bytes: #{res_sizes.min}\n"
 puts "Average resource size in bytes: #{res_sizes.reduce(:+).to_f / res_sizes.size}\n"
-puts "Failure URI: #{failure_uri.first} - #{failure_code.strip}" if failure_uri.empty? == false
+puts "Success Codes: #{success}\n"
+puts "Redirect Codes: #{redirects}\n"
+puts "Failure Codes: #{failures}\n\n"
+# If there are a ton of failures, this block may become unwieldy
+if failures != 0
+  failure_uri.each do |uri|
+    puts "Failure URI: #{uri}"
+  end
+end
